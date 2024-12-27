5d964f6892964e6dec821df29aba9b3ba5b6b234
Victor <yungaeronaut@gmail.com>
2024-12-27 23:42:27 +0100

    useEffect imported and deployed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+
+
 export default function App() {
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("http://localhost://5000/api/todos");
+      const todos = await res.json();
+      
+      console.log(todos);
+    }
+    getTodos();
+  }, [])
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className="container">
+      <h1>Awesome Todos</h1>
+    </main>
   );
 }
