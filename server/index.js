2b165286e352adca71f602d89fc961bea9c9a00c
Victor <yungaeronaut@gmail.com>
2025-01-01 19:29:24 +0100

    tidy up the index.html

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
+const path = require('path');
 
 const app= express();
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 const router = require("./routes");
 // app.get("/hello", (req, res) => {
